649dfe7ecc8a4b4a374fded233b17aa7a0944dc0
Daniel <danielrs9504@gmail.com>
2021-09-19 14:35:56 -0500

    Agrega placeholder al NewTodoForm

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -7,6 +7,7 @@ const NewTodoForm = () => {
     <div>
       <input
         type='text'
+        placeholder='Type your new Todo'
         value={inputValue}
         // El value va ser lo que sea que ingresemos en el text input
         onChange={(e) => setInputValue(e.target.value)}
