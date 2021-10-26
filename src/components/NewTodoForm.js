83af988907578c1d527293d6d3a7db3390d29dbd
Daniel <danielrs9504@gmail.com>
2021-10-26 18:11:52 -0500

    Conecta el componente al redux store

--- src/components/NewTodoForm.js
+++ src/components/NewTodoForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { createTodo } from '../actions/todoActions';
 
-const NewTodoForm = () => {
+const NewTodoForm = ({ todos, onCreatePressed }) => {
   const [inputValue, setInputValue] = useState('');
 
   return (
@@ -12,9 +14,29 @@ const NewTodoForm = () => {
         // El value va ser lo que sea que ingresemos en el text input
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button>Create Todo</button>
+      <button
+        onClick={() => {
+          const isDuplicateText = todos.some(
+            (todo) => todo.text === inputValue
+          );
+          if (!isDuplicateText) {
+            onCreatePressed(inputValue);
+            setInputValue('');
+          }
+        }}
+      >
+        Create Todo
+      </button>
     </div>
   );
 };
 
-export default NewTodoForm;
+const mapStateToProps = (state) => {
+  todos: state.todos;
+};
+
+const mapDispatchToProps = (dispatch) => {
+  onCreatePressed: (text) => dispatch(createTodo(text));
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
