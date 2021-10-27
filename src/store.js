d9018e3cb1d3ff26ecc85bf457f515117ffedd7c
Daniel <danielrs9504@gmail.com>
2021-10-27 07:49:11 -0500

    Conecta el Redux DevTools

--- src/store.js
+++ src/store.js
@@ -5,4 +5,8 @@ const reducers = { todoReducer };
 
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () =>
+  createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
