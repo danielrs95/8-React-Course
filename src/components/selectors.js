2290366e07cbd4636267fe83357638cce655ae57
Daniel <danielrs9504@gmail.com>
2021-11-22 08:26:15 -0500

    Agrega reselect y crea 2 nuevos selectores para sacar las listas de completed & incompleted

--- src/components/selectors.js
+++ src/components/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 export const getTodos = (state) => {
   return state.todos.data;
 };
@@ -5,3 +7,22 @@ export const getTodos = (state) => {
 export const getTodosLoading = (state) => {
   return state.todos.isLoading;
 };
+
+export const getIncompleteTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => !todo.isCompleted)
+);
+
+// Otra manera sería sacar la logica del filtro a otra funcion
+
+// const getIncompleteTodosFunction = (todos) => {
+//   return todos.filter((todo) => !todo.isCompleted);
+// };
+
+// export const getIncompleteTodos = createSelector(
+//   getTodos,
+//   getIncompleteTodosFunction
+// );
+
+export const getCompletedTodos = createSelector(getTodos, (todos) =>
+  todos.filter((todo) => todo.isCompleted)
+);
